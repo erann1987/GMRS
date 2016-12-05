ba4946138fad3b3728bb4c8f6bff0ada229fa1d4
erann1987 <erann1987@gmail.com>
2016-12-05 13:20:48 +0200

    second commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using GMRS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GMRS.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        public IEnumerable<Category> getCategories()
+        {
+            IEnumerable<Category> res;
+            using (var db = new GMRSDBEntities())
+            {
+                res = db.Category.ToList();
+            }
+            return res;
+        }
+    }
+}
